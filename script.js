2095bfdd2a3c3f8c4e9467dc04487cfcaae545a4
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-16 23:06:55 +0330

    mathematice operators

---
+++ script.js
@@ -0,0 +1,15 @@
+var firstNumber = 15
+var secondNumber = 15
+var thirdNumber = 3
+
+var plusResult = firstNumber + secondNumber
+
+var minusResult = firstNumber - secondNumber - thirdNumber
+
+var divideResult = firstNumber / secondNumber
+
+var multipliedResult = secondNumber * thirdNumber
+
+var tavanResult
+
+alert(multipliedResult)
