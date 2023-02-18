b0eb3618b8fe1e51d531093a07a4436fc3966d83
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-18 17:29:08 +0330

    tamrin vorudie user bareye raftan be Site

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-var userNumber = prompt("Enter your Number: ", 0)
+var userAge = +prompt("Enter your Age")
 
-if (userNumber % 2 == 0) {
-    
-alert("Yes It Is A EvenNumber" )
+if(userAge < 18) {
+
+alert("You Dont Have Permission")
+
 } else{
 
-   alert("No It Is A OddNumber" )
+alert("You Will Enter In site \n Just A Momment Please")
 
 }
-var userString = prompt("Enter your Name: ")
-
-console.log(typeof userString)
-if(typeof userString === "string") {
-
-alert("Your Name is Correct")
-
-} else {
-
-    alert("This It Not A Name.\n Maybe It is A Number \n Please Check Your Name Again")
-
-}
-var userNumber1 = +prompt("Enter your Number: ", 0)
-var userNumber2 = +prompt("Enter your Number: ", 0)
-var userNumber3 = +prompt("Enter your Number: ", 0)
-
-
- var Sum = userNumber1+userNumber2+userNumber3
- var Final=Sum / 3
-
- alert(Final)
