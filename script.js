433c3d7532975e3942ff1e6eca623d2bb0730df8
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-19 20:01:15 +0330

    tamrin if else va or (ya)

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-var userTime = Number(prompt("Please Enter Your Chosen Time"))
+ var Num1 = +prompt("Please Enter your First Number")
+var Num2 = +prompt("Please Enter your Second Number")
 
-var Time = userTime / 60
+var Total = Num1 * Num2
 
-if(isNaN(Time)){
+ if(Total < 0 ){
 
- alert("Please Enter Your Time Again")
+  alert("Your Answer is Under 0 Please Enter Another one")
 
-} else{
+ }
+ else if(isNaN(Total)){
+    alert("Please Enter Real Number")
+ }else{
+ alert("Your Final Answer is: " + Total) } 
+// OR || یا
+// true || true => true
+// true || false =>true
+// false || false => false 
 
-    alert("Your Time is: "+Time + " Hour")
+var num1 = 12
+var num2 = 4
+var num3 = 3
 
+if(num1 * num3 === 10 || num1 * num3 === 11){
+    alert("Your Answer is 10 Or 11")
+} else if (num2 * num3 === 13 || num1 * num3 === 15){
+alert("Your Answer is 13 Or 15")
+}else if(num2 * num3 === 14 || num2 * num3 === 12) {
+alert("Your Answer is 12 Or 14")
 }
+//////////////////////
 
-var UserBirthDate = Number(prompt("Please Enter Your Year Time", 1385))
-
-var UserAge = 1401 - UserBirthDate
-
-if(isNaN(UserBirthDate)) {
-    
-    alert("Please Enter Your True Birth Date like: 1385")
-
-  }else if(UserAge <0){
-
-      alert("This Date is Not Correct")
-    }
-    else{
-        
-        alert("Your Birth Date is: " + UserAge)
-}
