07ad5530042e758be897740fad9b8b52b3decd06
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-19 20:22:08 +0330

    tamrin sath dastresi gereftan sen karbar va ejaze dadan be karbar baraye vorod be site

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
- var Num1 = +prompt("Please Enter your First Number")
-var Num2 = +prompt("Please Enter your Second Number")
+var UserAge = +prompt("Please Enter your Age")
+var UserGender = prompt("Please Enter your Gender: (Male,Female)")
 
-var Total = Num1 * Num2
+if(UserGender == "Female" || UserAge < 18){
 
- if(Total < 0 ){
+    alert("This Site is Not Good for you")
 
-  alert("Your Answer is Under 0 Please Enter Another one")
-
- }
- else if(isNaN(Total)){
-    alert("Please Enter Real Number")
- }else{
- alert("Your Final Answer is: " + Total) } 
-// OR || یا
-// true || true => true
-// true || false =>true
-// false || false => false 
-
-var num1 = 12
-var num2 = 4
-var num3 = 3
-
-if(num1 * num3 === 10 || num1 * num3 === 11){
-    alert("Your Answer is 10 Or 11")
-} else if (num2 * num3 === 13 || num1 * num3 === 15){
-alert("Your Answer is 13 Or 15")
-}else if(num2 * num3 === 14 || num2 * num3 === 12) {
-alert("Your Answer is 12 Or 14")
+}else {
+    alert("You Have A Permission To Enter This site ")
 }
-//////////////////////
-
