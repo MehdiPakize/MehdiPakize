131e4d4d3c4e72e552c125e58ba61edc486adc25
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-20 22:07:32 +0330

    tamrin Ternary

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-// var num2 = 4
-// var num3 = 3
+var num2 = 4
+var num3 = 3
 
-// if(num2 === 3 && num2 * num3 === 12){
-// alert("num2= 3  And Your Answer is True")
-// }else if(num3 === 3 && num2 * num3 === 12){
-//     alert("num3= 4  And Your Answer is True")
-// }else {
-//     alert("Your Answer is Wrong")
-// }
+num2 * num3 === 12 ?  alert("Yes  3 time 4 = 12") : alert("No The answer is Wrong")
 
-////////////////////////
+var N1 = 15
+var N2 = 3
+var N3 = 7
+var N4 = 8
 
-var mybool1 = true
-alert("mybool1 = "+ mybool1)
-alert("!mybool1 = "+ !mybool1)
-alert("!!mybool = " + !!mybool1)
+N1 * N2 === 45 || N3 * N4 === 56 ? alert("Your caculation is True") : alert("This is Not true")
+
+var UserNumber1 = +prompt("Please Enter your First Number")
+var UserNumber2 = +prompt("Please Enter your Second Number")
+
+var Final = UserNumber1 * UserNumber2
+UserNumber1 * UserNumber2 % 2 === 0 || UserNumber1 * UserNumber2 % 2 === 2 ? alert("Your Answer Is: "+Final ) : alert("Your Answer is Wrong")
+if(isNaN(Final)){
+    alert("Please Enter your Number Again")
+}
