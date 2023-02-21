013cc7d110021a9a636eb67cf0d708cc4d21616c
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-21 19:45:01 +0330

    tamrin switch case

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-var num1 = 3
-var num2 = 4
+// var num1 = 3
+// var num2 = 4
 
-var num3 = num1 *num2
+// var num3 = num1 *num2
 
-switch(num3){
-case 10 :
-    alert("Num3 = 10")
-    break;
-    case 11 : 
-    alert("Num3 = 11")
-    break;
-    case 12 :
-    case 15 :
-    alert ("Num3 = 12 or 15")
-    break;
-    case 13 : 
-    alert ("Num3 = 13")
-    break;
-    case 14 : 
-    alert ("Num3 = 14")
-    break;
-    default : alert("Num3 dont have a answer :(")
-    break;
+// switch(num3){
+// case 10 :
+//     alert("Num3 = 10")
+//     break;
+//     case 11 : 
+//     alert("Num3 = 11")
+//     break;
+//     case 12 :
+//     case 15 :
+//     alert ("Num3 = 12 or 15")
+//     break;
+//     case 13 : 
+//     alert ("Num3 = 13")
+//     break;
+//     case 14 : 
+//     alert ("Num3 = 14")
+//     break;
+//     default : alert("Num3 dont have a answer :(")
+//     break;
+// }
+var userGpa = +prompt("Please Enter your Gpa")
+switch(userGpa){
+    case 18 :
+        case 19 :
+            case 20 :
+        alert("Your Result is A")
+        break;
+        case 15 :
+            case 16 :
+                case 17 :
+        alert("Your Result is B")
+        break;
+        case 12:
+            case 13 : 
+            case 14 :
+        alert("Your Result is C")
+        break
+        case 12 : 
+        alert("You Failed This Term")
+        case (isNaN(userGpa)):
+            alert("Please Try Again")
+            default : alert("You Failed This Term ")
 }
