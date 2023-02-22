feb757481fedae74ad83528d05166e001f9ff251
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-22 19:00:06 +0330

    tamrin hale tamrin zoj ya fard boodan adad varudi be komak tabeh

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
- function ShowText(usertext) {
- alert("Your Parameter is: " + usertext)
+  function ShowText(usertext) {
+  alert("Your Parameter is: " + usertext)
+  }
+  ShowText("SabzLearn.ir 0_0")
+ function showTwoNumbers (firstNumber, secondNumber){
+     alert("First Parameter: " + firstNumber)
+     alert("Second Parameter: " + secondNumber)
  }
- ShowText("SabzLearn.ir 0_0")
-function showTwoNumbers (firstNumber, secondNumber){
-    alert("First Parameter: " + firstNumber)
-    alert("Second Parameter: " + secondNumber)
+ showTwoNumbers(34 , 18)
+/////////////////////
+
+function userShowNumber (UserNumber){
+   var UserNumber = +prompt("Please Enter Your Number")
+   if(UserNumber % 2 === 0){
+    alert("Your Number is: " + UserNumber + " Even Number")
+   }else if (UserNumber % 2 === 1) {
+    alert("Your Number is: " + UserNumber + " Odd Number")
+   }else {
+    alert("Maybe This is Not A Number Pleas Try Again")
+   }
 }
-showTwoNumbers(34 , 18)
+userShowNumber("UserNumber")
