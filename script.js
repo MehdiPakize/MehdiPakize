9ae71f7688dcdee81356bdb12b0c4992c39f6cf1
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-23 19:52:47 +0330

    hale tamrin mohasebe miangin be komak tabeh

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-  function ShowText(usertext) {
-  alert("Your Parameter is: " + usertext)
-  }
-  ShowText("SabzLearn.ir 0_0")
- function showTwoNumbers (firstNumber, secondNumber){
-     alert("First Parameter: " + firstNumber)
-     alert("Second Parameter: " + secondNumber)
- }
- showTwoNumbers(34 , 18)
-/////////////////////
 
-function userShowNumber (UserNumber){
-   var UserNumber = +prompt("Please Enter Your Number")
-   if(UserNumber % 2 === 0){
-    alert("Your Number is: " + UserNumber + " Even Number")
-   }else if (UserNumber % 2 === 1) {
-    alert("Your Number is: " + UserNumber + " Odd Number")
-   }else {
-    alert("Maybe This is Not A Number Pleas Try Again")
-   }
+//  function userShowNumber (){
+//     var UserNumber = +prompt("Please Enter Your Number")
+//     if(UserNumber % 2 === 0){
+//      alert("Your Number is: " + UserNumber + " Even Number")
+//     }else if (UserNumber % 2 === 1) {
+//      alert("Your Number is: " + UserNumber + " Odd Number")
+//     }else {
+//      alert("Maybe This is Not A Number Pleas Try Again")
+//     }
+//  }
+//  userShowNumber()
+// function mianginNumber (){
+//   var num1 = 1
+//   var num2 = 2
+//   var num3 = 3
+//   var finalResult = (num1 + num2 + num3) / 3
+//   alert("Your answer is: " + finalResult)
+// }
+// mianginNumber()
+
+
+function SumResult(num1=3 , num2=6){
+var sum = num1 + num2
+alert(sum)
 }
-userShowNumber("UserNumber")
+SumResult(5)
