13bca3da8543e5dc0e2340eff2e0d953bd6f680a
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-23 21:31:06 +0330

    tamrin kalameye Return

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
- function sumNumbers (num1 , num2){
-   var result = num1 + num2
-   // alert("Result: " + result)
-   return result
- }
- var sumResult = sumNumbers(2,8)
+//  function sumNumbers (num1 , num2){
+//    var result = num1 + num2
+//    // alert("Result: " + result)
+//    return result
+//  }
+//  var sumResult = sumNumbers(2,8)
 
- alert("sumResult: " + sumResult) 
+//  alert("sumResult: " + sumResult) 
 
-var number1 = prompt("Please Enter your FirstNumber")
-var number2 = prompt("Please Enter your SecondNumber")
+// var number1 = prompt("Please Enter your FirstNumber")
+// var number2 = prompt("Please Enter your SecondNumber")
 
 
 
-function showPower (num1 ,num2){
-return num1 ** num2 
+// function showPower (num1 ,num2){
+// return num1 ** num2 
+// }
+
+// var powerResult = showPower(number1,number2)
+
+// alert("PowerResult = " + powerResult)
+
+var Number1 = prompt("Please Enter your FirstNumber")
+var Number2 = prompt("Please Enter your SecondNumber")
+
+
+function UserNumber (Number1 , Number2){
+if(isNaN(Number1 && Number2)){
+alert("Please Try Again")
 }
-
-var powerResult = showPower(number1,number2)
-
-alert("PowerResult = " + powerResult)
+return Number1 / Number2
+}
+var Result = UserNumber(Number1,Number2)
+alert("Your Final Answer Is: " + Result)
