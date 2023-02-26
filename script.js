11d1b4d3cf5be48208da40ff3e623e255816d5d7
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-26 19:16:42 +0330

    tamrin function9

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-// ye meghdari ro az karbari migirim ke agar meghdaresh ba meghdar user name barabar bood is login beshe true, function welcome run beshe, agar barabar nabood function try again anjam beshe
-var UserName = "ali"
-var isLogin = false
-var password = 123
+// // ye meghdari ro az karbari migirim ke agar meghdaresh ba meghdar user name barabar bood is login beshe true, function welcome run beshe, agar barabar nabood function try again anjam beshe
+// var UserName = "ali"
+// var isLogin = false
+// var password = 123
 
-var User = prompt("Please Enter Your Number")
-
-
-function userNameValidation (userInputValidationFunction){
-// baresi vorudi username
-if(userInputValidationFunction == true){
-  isLogin = true
-
-  PassWordValidation()
-}else{
-  isLogin = false
-}
-}
+// var User = prompt("Please Enter Your Number")
 
 
-function PassWordValidation (){
-// baresi dorost budan password
-  var UserPassword = +prompt("Please Enter Your PassWord")
-if(UserPassword == password){
-successLogin()
-}else{
-  BlockLogin()
-}
-}
+// function userNameValidation (userInputValidationFunction){
+// // baresi vorudi username
+// if(userInputValidationFunction == true){
+//   isLogin = true
+
+//   PassWordValidation()
+// }else{
+//   isLogin = false
+// }
+// }
 
 
-function successLogin (){
-// movafaghiat haye user
-alert("Your Login is successfull")
-}
+// function PassWordValidation (){
+// // baresi dorost budan password
+//   var UserPassword = +prompt("Please Enter Your PassWord")
+// if(UserPassword == password){
+// successLogin()
+// }else{
+//   BlockLogin()
+// }
+// }
+
+
+// function successLogin (){
+// // movafaghiat haye user
+// alert("Your Login is successfull")
+// }
 
 
 
-function BlockLogin (){
-// failed dadan login
-alert("Afghani PassWord Eshtebahe")
-}
+// function BlockLogin (){
+// // failed dadan login
+// alert("Afghani PassWord Eshtebahe")
+// }
 
-let CheckisLoginValidation = (Value,Valuetarget) => {
-var isLoginValue = null
-if(Value==Valuetarget){
-isLoginValue = true
-}else{
-  isLoginValue = false
-}
-return isLoginValue
-}
+// let CheckisLoginValidation = (Value,Valuetarget) => {
+// var isLoginValue = null
+// if(Value==Valuetarget){
+// isLoginValue = true
+// }else{
+//   isLoginValue = false
+// }
+// return isLoginValue
+// }
 
 userNameValidation (CheckisLoginValidation(User,UserName))
