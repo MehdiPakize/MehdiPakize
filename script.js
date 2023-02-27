d7ae31fc66ff087778f19e59dab7cfd870f2d6c5
Mehdi pakize <m.pakize1383@gmail.com>
2023-02-27 23:04:35 +0330

    tamrin function14

--- script.js
+++ script.js
@@ -18,15 +18,15 @@ if(userInputValidationFunction == true){
 }
 
 
-function PassWordValidation (){
-// baresi dorost budan password
-  var UserPassword = +prompt("Please Enter Your PassWord")
-if(UserPassword == password){
-successLogin()
-}else{
-  BlockLogin()
-}
-}
+// function PassWordValidation (){
+// // baresi dorost budan password
+//   var UserPassword = +prompt("Please Enter Your PassWord")
+// if(UserPassword == password){
+// successLogin()
+// }else{
+//   BlockLogin()
+// }
+// }
 
 
 // function successLogin (){
